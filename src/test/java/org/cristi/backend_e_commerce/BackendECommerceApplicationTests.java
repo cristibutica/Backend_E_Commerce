7b2700aa64f1ac818177ef09694ab282de2024e9
Cristi Butica <Butica.Vi.Cristian@student.utcluj.ro>
2024-08-31 23:22:18 +0300

    Initialized Spring Boot project.

---
+++ src/test/java/org/cristi/backend_e_commerce/BackendECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cristi.backend_e_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendECommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
